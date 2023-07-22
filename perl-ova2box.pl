f093dec7e06e264ed48ae4a3dc4a42eb875f1ae4
Yoshinori Takesako <takesako@namazu.org>
2023-07-22 15:17:19 +0900

    fix file permission

--- perl-ova2box.pl
+++ perl-ova2box.pl
@@ -4,6 +4,10 @@ use warnings;
 
 use Archive::Tar;
 
+my $mode = '0644';
+my $owner = 'root:root';
+my $file;
+
 sub trim_ovf {
   my $ovf = shift;
   $ovf =~ s#<ExtraData>.*?</ExtraData>\s*##s;
@@ -20,16 +24,25 @@ my $arg_box = "$arg.box";
    $arg_box = $ARGV[1] if (defined $ARGV[1]);
 
 my $tar = Archive::Tar->new($arg_ova) or die;
-foreach my $file ($tar->list_files()) {
+
+foreach $file ($tar->list_files()) {
   print STDERR "$file...\n";
   if ($file =~ /\.ovf$/) {
     my $ovf = $tar->get_content($file);
     $tar->remove($file);
-    $tar->add_data('box.ovf', trim_ovf($ovf));
+    $tar->add_data($file = 'box.ovf', trim_ovf($ovf));
   }
+  $tar->chmod($file, $mode);
+  $tar->chown($file, $owner);
 }
-$tar->add_data('metadata.json', '{"provider": "virtualbox"}');
-$tar->add_data('Vagrantfile', '');
+
+$tar->add_data($file = 'metadata.json', '{"provider": "virtualbox"}');
+$tar->chmod($file, $mode);
+$tar->chown($file, $owner);
+$tar->add_data($file = 'Vagrantfile', '');
+$tar->chmod($file, $mode);
+$tar->chown($file, $owner);
+
 $tar->write($arg_box, COMPRESS_GZIP);
 
 1;
