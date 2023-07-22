945ccc9bc010471b38ac00b8587b79afd242f3d1
Yoshinori Takesako <takesako@namazu.org>
2023-07-22 23:38:13 +0900

    vix for folder sync

--- x-only-vmware.sh
+++ x-only-vmware.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -x
 arch=`cat /etc/apk/arch`
 if [ "$arch" == "aarch64" ] || [ "$arch" == "x86_64" ]; then
-  apk add open-vm-tools-hgfs open-vm-tools-guestinfo
+  apk add open-vm-tools-hgfs open-vm-tools-vix
   rc-update add open-vm-tools
   echo fuse > /etc/modules-load.d/open-vm-tools.conf
 else
