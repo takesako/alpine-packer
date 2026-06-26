6b40dc3b0bce41ab89ea7e09e26575ff704ef392
Yoshinori Takesako <takesako@gmail.com>
2026-06-26 12:33:05 +0900

    use hgfs

--- x-only-vmware.sh
+++ x-only-vmware.sh
@@ -1,9 +1,11 @@
 #!/bin/sh -x
 arch=`cat /etc/apk/arch`
 if [ "$arch" == "aarch64" ] || [ "$arch" == "x86_64" ]; then
-  apk add open-vm-tools-hgfs open-vm-tools-vix
-  rc-update add open-vm-tools
+  apk add --no-cache open-vm-tools-hgfs open-vm-tools-vix open-vm-tools-guestinfo fuse3
   echo fuse > /etc/modules-load.d/open-vm-tools.conf
+  rc-update add open-vm-tools default || true
+  rc-service open-vm-tools start || true
+  which vmhgfs-fuse || true
 else
   echo "$arch: open-vm-tools is not support 32bit"
 fi
@@ -16,4 +18,4 @@ if [ "$arch" == "aarch64" ]; then
       /sbin/mkinitfs
     fi
   fi
-fi                                                              
+fi
